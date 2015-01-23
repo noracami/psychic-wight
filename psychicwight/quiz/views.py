79b212a46246ca893a0cc8da3485b4c22c278a84
noracami <illuwion@gmail.com>
2015-01-24 00:46:54 +0800

    ready to bed

--- psychicwight/quiz/views.py
+++ psychicwight/quiz/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
+import os
+import csv
 
 # Create your views here.
 def home(request):
     h = "%s%s%s" % ("○○部○○署", 104, "年度○○裝備檢查保養常識測驗")
+    module_dir = os.path.join(os.path.dirname(__file__), '../static/car1.csv')
+    with open(module_dir, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar=',')
+        i = 1
+        text3 = []
+        for row in spamreader:
+            clist = []
+            for s in row:
+                for c in s.split(','):
+                    if c == "":
+                        pass
+                    else:
+                        clist += [c]
+            i += 1
+            text3 += [clist]
+    return render(request, 'home.html', {
+        "header": h,
+        "m": module_dir,
+        "text3": text3,
+        })
     text = {
         "t1": "【車輛裝備】保養常識練習題庫（選擇50題）",
         "t2": "【車輛裝備】保養常識練習題庫（是非50題）",
@@ -19,7 +41,3 @@ def home(request):
              "【通訊裝備】保養常識練習題庫（選擇50題）", "【通訊裝備】保養常識練習題庫（是非50題）",
              "【防彈裝備】保養常識練習題庫（選擇20題）", "【防彈裝備】保養常識練習題庫（是非20題）",
              "模擬測驗(隨機20題)",]
-    return render(request, 'home.html', {
-        "header": h,
-        "text2": text2,
-        })
